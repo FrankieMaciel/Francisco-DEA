663478c980eebb9fa34bed5a797fc7154c8a376e
aaFranks <110301698+FrankieMaciel@users.noreply.github.com>
2024-08-15 10:59:59 -0300

    feat: installation scripts

---
+++ install_libraries.bat
@@ -0,0 +1,6 @@
+@echo off
+set CRAN_REPO=https://cloud.r-project.org/
+Rscript -e "install.packages(c('lintr', 'shiny', 'shinycssloaders', 'pracma', 'fractaldim', 'openxlsx', 'shinyjs', 'Benchmarking', 'shinydashboard', 'shinyalert', 'DT', 'TSA', 'longmemo', 'plyr', 'shinyFiles', 'tools', 'htmltools', 'markdown'), repos='%CRAN_REPO%')"
+Rscript -e "install.packages('C:/path/to/ptsuite.tar.gz', repos = NULL, type = 'source')"
+echo Instalação concluída.
+pause
